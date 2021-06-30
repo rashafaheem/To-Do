7a154cccd5032960095ea274d4f7395d411f9596
Rasha Faheem <64769011+rashafaheem@users.noreply.github.com>
2021-06-30 04:07:04 -0700

    Design Update

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ function App() {
   return (
     <div className="app">
       <div className="mainHeading">
-        <h1>ToDo List</h1>
+        <h1>To-Do List</h1>
       </div>
       <div className="subHeading">
         <br />
-        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+        <h2>Make your day productive</h2>
       </div>
       <div className="input">
         <input value= {toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
