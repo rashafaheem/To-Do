57990ebfb359b26edcbc1a35dfe0a71c933ad70b
Rasha Faheem <64769011+rashafaheem@users.noreply.github.com>
2021-06-30 04:16:25 -0700

    Deleting tasks

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import {useState} from 'react'
 function App() {
   const [toDos,setToDos] = useState([])
   const [toDo,setToDo] = useState('')
+  const [cancelled, setCancelled] = useState([]);
+
   return (
     <div className="app">
       <div className="mainHeading">
@@ -19,27 +21,51 @@ function App() {
         <i onClick={()=>setToDos([...toDos,{id:Date.now(), text: toDo,status:false}])} className="fas fa-plus"></i>
       </div>
       <div className="todos">
-      {
-        toDos.map((obj)=>{
-        return (<div className="todo">
-          <div className="left">
-            <input onChange={(e)=>{
-              console.log(e.target.checked)
-              console.log(obj)
-              setToDos(toDos.filter(obj2=>{
-                if(obj2.id===obj.id){
-                  obj2.status=e.target.checked
-                }
-                return obj2
-              }))
-            }} 
-            value={obj.status} type="checkbox" name="" id="" />
-            <p>{obj.text}</p>
-          </div>
-          <div className="right">
-            <i className="fas fa-times"></i>
-          </div>
-        </div>)
+        {toDos.map((obj) => {
+          if (obj.deleted !== true) {
+            return (
+              <div key={obj.id} className="todo">
+                <div className="left">
+                  <input
+                    onChange={(e) => {
+                      setToDos(
+                        toDos.filter((toDoItem) => {
+                          if (toDoItem.id === obj.id) {
+                            toDoItem.status = e.target.checked;
+                          }
+                          return toDoItem;
+                        })
+                      );
+                    }}
+                    value={obj.status}
+                    className="strike-through"
+                    type="checkbox"
+                    name=""
+                    id=""
+                  />
+                  <p>{obj.text}</p>
+                </div>
+                <div className="right">
+                  <i
+                    className="fas fa-times"
+                    onClick={() => {
+                      setToDos(
+                        toDos.filter((toDoItem) => {
+                          if (toDoItem.id === obj.id) {
+                            toDoItem.deleted = true;
+                            !toDoItem.status &&
+                              setCancelled([...cancelled, toDoItem.text]);
+                          }
+                          return toDoItem;
+                        })
+                      );
+                    }}
+                  ></i>
+                </div>
+              </div>
+            );
+          }
+          return null
         })}
         <br></br>
         <h1>Completed Tasks</h1>
